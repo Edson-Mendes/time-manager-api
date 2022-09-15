81e59893626b0fbb091ee9ee314809ac7d5cad08
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-09-15 08:41:56 -0300

    Adicionado configuração de CORS

---
+++ src/main/java/br/com/emendes/timemanagerapi/config/CORSConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.emendes.timemanagerapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/activities").allowedOrigins("http://127.0.0.1:5500")
+            .allowedMethods("GET", "POST");
+        registry.addMapping("/activities/*").allowedOrigins("http://127.0.0.1:5500")
+            .allowedMethods("DELETE", "PUT");
+      }
+    };
+  }
+
+}
